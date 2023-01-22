f4c187172112c438610b86d3e93bcc91c7f45774
BuildTools <unconfigured@null.spigotmc.org>
2023-01-21 22:16:50 -0700

    Added middleware and effectively made the Thunk that we worked so hard on useless

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+// import { useEffect } from 'react';
+import { useDispatch, useSelector, connect } from 'react-redux';
 import { fetchData, incrementId, decrementId, clearData, setData, customId } from './ducks/dataSlice';
+
+const mapStateToProps = (state) => ({
+  objectId: state.data.objectId
+})
 
 function App(props) {
   const dispatch = useDispatch()
@@ -14,15 +18,20 @@ function App(props) {
       return <p>Not found!</p>
     }
   }
+  
+//   useEffect(() => {
+//     dispatch(fetchData())
+// }, [props.objectId, dispatch])
+
 
   console.log(data)
   return (
     <div className="App">
       <div>
         <button onClick={() => dispatch(fetchData())}>Thunk!</button>
-        <button onClick={() => dispatch(clearData())}>Clear</button>
-        <button onClick={() => dispatch(incrementId())}>Next</button>
         <button onClick={() => dispatch(decrementId())}>Back</button>
+        <button onClick={() => dispatch(incrementId())}>Next</button>
+        <button onClick={() => dispatch(clearData())}>Clear</button>
       </div>
       <div>
         {data.objectId}
@@ -32,4 +41,4 @@ function App(props) {
   );
 }
 
-export default App
+export default connect(mapStateToProps)(App)
--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { logger } from './ducks/middleware'
 import dataReducer from './ducks/dataSlice'
 
 export const store = configureStore({
     reducer: {
         data: dataReducer
-    }
+    },
+    middleware: [ logger ]
 })
